1ba007ed0119235a533f27bc273c2436dff48514
Andrew Pettit <andrewp2010@gmail.com>
2022-11-03 15:47:53 -0700

    check for /bin, add output

--- scripts/osl2hda.py
+++ scripts/osl2hda.py
@@ -1,20 +1,32 @@
-import logging, os, rfh
+import ap, os, rfh
 
 filePath = os.path.dirname(os.path.realpath(__file__))
 root, _ = os.path.split(filePath)
 
 root = root.replace(os.sep, '/')
 
+# check for /bin dir
+if (os.path.isdir("bin") == False):
+    ap.print.info("make /bin")
+    os.mkdir("bin")
+
 shaderPaths = [(root + "/bin")]
 libName = 'APETTIT_OSL_LIB'
 otlLibFile = root + "/otls/" + libName + ".hda"
 iconPath = root + "/src/icon/apettit_icon.png"
 
+# remove old lib instance
 if (os.path.exists(otlLibFile)):
+    print("remove old APETTIT_OSL_LIB inst")
     os.remove(otlLibFile)
 
-rfh.convertutils.main(shaderPaths,
+processLog = rfh.convertutils.main(shaderPaths,
                         otlfile = otlLibFile,
                         icon = iconPath,
                         menu = "apettit",
                         originalCase = True)
+
+# output
+if(processLog == None):
+    ap.print.success(("SUCCESSFULLY converted %s:" % libName))
+    ap.print.info(("otls/%s.hda" % libName))
