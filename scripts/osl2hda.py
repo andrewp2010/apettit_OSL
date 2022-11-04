fcb1ec6298a41bab3826b074e5e08289daaba110
Andrew Pettit <andrewp2010@gmail.com>
2022-11-03 23:18:00 -0700

    added RMAN_SHADERPATH ref

--- scripts/osl2hda.py
+++ scripts/osl2hda.py
@@ -1,30 +1,37 @@
-import ap, os, rfh
-
-filePath = os.path.dirname(os.path.realpath(__file__))
-root, _ = os.path.split(filePath)
-
-root = root.replace(os.sep, '/')
+import ap, os, platform, rfh
 
 # check for /bin dir
 if (os.path.isdir("bin") == False):
     ap.print.info("make /bin")
     os.mkdir("bin")
 
-shaderPaths = [(root + "/bin")]
+shaderPaths = ["bin"]
 libName = 'APETTIT_OSL_LIB'
-otlLibFile = root + "/otls/" + libName + ".hda"
-iconPath = root + "/src/icon/apettit_icon.png"
+otlLibFile = "otls/" + libName + ".hda"
+iconPath = "src/icon/apettit_icon.png"
 
-# remove old lib instance
+# remove old lib hda
 if (os.path.exists(otlLibFile)):
     print("remove old APETTIT_OSL_LIB inst")
     os.remove(otlLibFile)
 
-processLog = rfh.convertutils.main(shaderPaths,
-                        otlfile = otlLibFile,
-                        icon = iconPath,
-                        menu = "apettit",
-                        originalCase = True)
+envs = ['RMAN_SHADERPATH']
+
+for env in envs:
+    try:
+        envpath = os.environ.get(env)
+        if envpath:
+            if platform.system() == 'Windows':
+                shaderpaths = envpath.split(';')
+            else:
+                shaderpaths = envpath.split(':')
+            processLog = rfh.convertutils.main(shaderPaths,
+                                               otlfile = otlLibFile,
+                                               icon = iconPath,
+                                               menu = "apettit",
+                                            originalCase = True)
+    except:
+        ap.print.error("RFH: Could not register %s" % otlLibFile)
 
 # output
 if(processLog == None):
