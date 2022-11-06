d5a29862764a31af71003317644b817dee570e1d
Andrew Pettit <andrewp2010@gmail.com>
2022-11-05 17:16:38 -0700

    initial commit

---
+++ src/lib/Vector.h
@@ -0,0 +1,14 @@
+#include "Params.h"
+
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#define VECTOR_DB vector(0.0, 0.0, 0.0)
+
+#define VECTOR_PARAM_INPUTS \
+    VECTOR("", inputV, VECTOR_DB, string label = "Vector")
+
+#define VECTOR_PARAM_OUTPUTS \
+    OUT_FLOAT(resultV, 0.0)
+
+#endif // VECTOR_H              
